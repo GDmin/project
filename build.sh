b80654eb3f02c010e1d115c3cd953416b209e427
GDmin <minco.0418@gmail.com>
2020-04-08 14:25:51 +0800

    update build place

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ set -ex
 #set registry
 USERNAME=396654005341.dkr.ecr.us-east-1.amazonaws.com
 # image name
-IMAGE=devops-hw-5
+IMAGE=test_project
 docker build -t $USERNAME/$IMAGE:latest .
 docker push $USERNAME/$IMAGE:latest
 
